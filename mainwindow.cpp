c98953c6a559602577669989a107b6ad3e4c7dc5
Luis <Luisf311220@gmail.com>
2024-09-01 11:04:59 -0300

    Primeiro programa no QT

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,15 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->label->setText("Obrigado Por Contribuir");
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->label->setText("https://github.com/Luisf66/Qt-Creator.git");
+}
+
