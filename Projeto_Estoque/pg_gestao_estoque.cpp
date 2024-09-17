4e6339e19f84c1510e3ea25e2ed41de0a563814c
Luisf66 <Luisf311220@gmail.com>
2024-09-17 20:34:55 -0300

    Inicialização da tela gestão estoque

--- Projeto_Estoque/pg_gestao_estoque.cpp
+++ Projeto_Estoque/pg_gestao_estoque.cpp
@@ -12,3 +12,25 @@ pg_gestao_estoque::~pg_gestao_estoque()
 {
     delete ui;
 }
+
+void pg_gestao_estoque::on_btn_novo_produto_clicked()
+{
+    // limpar os campos
+    ui->campo_codigo->clear();
+    ui->campo_descricao->clear();
+    ui->campo_fornecedor->clear();
+    ui->campo_quantidade->clear();
+    ui->campo_valor_compra->clear();
+    ui->campo_valor_venda->clear();
+    // apontar para o campo codigo apos limpeza dos campos
+    ui->campo_codigo->setFocus();
+
+}
+
+
+void pg_gestao_estoque::on_btn_salvar_clicked()
+{
+    //QString ;
+    QDebug() << "Botão de Salvar";
+}
+
--- Projeto_Estoque/pg_gestao_estoque.h
+++ Projeto_Estoque/pg_gestao_estoque.h
@@ -15,6 +15,11 @@ public:
     explicit pg_gestao_estoque(QWidget *parent = nullptr);
     ~pg_gestao_estoque();
 
+private slots:
+    void on_btn_novo_produto_clicked();
+
+    void on_btn_salvar_clicked();
+
 private:
     Ui::pg_gestao_estoque *ui;
 };
